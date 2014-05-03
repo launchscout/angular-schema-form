b35272b7a82c83b1bd78644a7461047ed8dd708c
Chris Nelson <chris@gaslightsoftware.com>
2014-05-02 20:47:48 -0400

    fixing boo boo and rebuilding dist

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -11,16 +11,28 @@
   angular.module("schemaForm").directive("schemaFormField", function($compile, $templateCache) {
     return {
       restrict: "EA",
+      replace: true,
+      require: "^form",
       scope: {
         schema: "=",
         model: "=",
         field: "=",
+        required: "=",
         editAs: "@"
       },
-      link: function(scope, element, attrs) {
+      link: function(scope, element, attrs, formController) {
         var template;
+        scope.formState = formController;
         template = scope.schema["enum"] != null ? $templateCache.get("enumField.html") : $templateCache.get("" + scope.schema.type + "Field.html");
         element.html(template);
+        if (scope.schema.type === "number" || scope.schema.type === "integer") {
+          element.find("input").attr("type", "number");
+        }
+        element.find("input").attr("ng-required", scope.required);
+        if (scope.schema.pattern) {
+          element.find("input").attr("ng-pattern", "/" + scope.schema.pattern + "/");
+        }
+        element.find("input").attr("name", scope.field);
         return $compile(element.contents())(scope);
       }
     };
@@ -34,7 +46,13 @@
       restrict: "EA",
       scope: {
         schema: "=",
-        model: "="
+        model: "=",
+        fields: "="
+      },
+      link: function(scope, element, attrs) {
+        return scope.required = function(field) {
+          return _.contains(this.schema.required, field);
+        };
       },
       templateUrl: "schemaFormFields.html"
     };
@@ -45,29 +63,32 @@
 angular.module("schemaForm").run(["$templateCache", function($templateCache) {
 
   $templateCache.put("booleanField.html",
-    "<div class=\"formgroup\">\n" +
-    "  <label>{{schema.title}}</label>\n" +
-    "  <input name=\"field\" ng-model=\"model[field]\" type=\"checkbox\"></input>\n" +
+    "<div class=\"form-group\">\n" +
+    "  <label for=\"{{field}}\">{{schema.title}}</label>\n" +
+    "  <input id=\"{{field}}\" name=\"field\" ng-model=\"model[field]\" type=\"checkbox\"></input>\n" +
     "</div>\n"
   );
 
   $templateCache.put("enumField.html",
-    "<div class=\"formgroup\">\n" +
-    "  <label>{{schema.title}}</label>\n" +
-    "  <select name=\"{{field}}\" ng-model=\"model[field]\" ng-options=\"enumValue for enumValue in schema.enum\"></select>\n" +
+    "<div class=\"form-group\">\n" +
+    "  <label for=\"{{field}}\">{{schema.title}}</label>\n" +
+    "  <select id=\"{{field}}\" name=\"{{field}}\" ng-model=\"model[field]\" ng-options=\"enumValue for enumValue in schema.enum\"></select>\n" +
     "</div>\n"
   );
 
   $templateCache.put("schemaFormFields.html",
-    "<div ng-repeat=\"(field, fieldSchema) in schema.properties\">\n" +
-    "  <schema-form-field field=\"field\" schema=\"fieldSchema\" model=\"model\"></schema-form-field>\n" +
+    "<div ng-repeat=\"field in fields\">\n" +
+    "  <schema-form-field field=\"field\"\n" +
+    "    schema=\"schema.properties[field]\"\n" +
+    "    model=\"model\" required=\"required(field)\">\n" +
+    "  </schema-form-field>\n" +
     "</div>\n"
   );
 
   $templateCache.put("stringField.html",
-    "<div class=\"form-group\">\n" +
-    "  <label for=\"{{field}}\">{{schema.title}}</label>\n" +
-    "  <input id=\"{{field}}\" name=\"{{field}}\" ng-model=\"model[field]\" type=\"text\" class=\"form-control\"></input>\n" +
+    "<div class=\"form-group\" ng-class=\"{'has-error': formState[field].$invalid}\">\n" +
+    "  <label for=\"{{field}}\" class=\"control-label\">{{schema.title}}</label>\n" +
+    "  <input id=\"{{field}}\" ng-attr-name=\"{{field}}\" ng-model=\"model[field]\" type=\"text\" class=\"form-control\"></input>\n" +
     "</div>\n"
   );
 
